213f75d4915b322e1695b7cd85d9d0f8f70d4f54
Pedro Minicz <pedrominicz@gmail.com>
2020-05-29 20:53:28 -0300

    Add Bootstrap.

--- html/Respostas.php
+++ html/Respostas.php
@@ -8,10 +8,33 @@ use Slim\Views\Twig;
 
 final class Respostas {
 
+  private $questions = [
+    'Em ambiente profissional, seu desempenho é melhor trabalhando com autonomia ou sobre orientação constante?',
+    'Conte sobre algum objetivo que perseguiu. O que sacrificou? Conseguiu o esperado?',
+    'Como será o mundo pós-coronavírus? Quais ganhos nossa sociedade pode ter?',
+    'Qual seu grau de conhecimento do sistema operacional Microsoft Windows?',
+    'Qual seu grau de conhecimento do sistema operacional Linux?',
+    'Qual seu grau de conhecimento de redes TCP/IP?',
+    'Qual seu grau de conhecimento de cloud computing?',
+  ];
+
+  private $answers = [
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+  ];
+
   public function __invoke(ServerRequestInterface $request, ResponseInterface $response) {
     $view = Twig::fromRequest($request);
 
-    return $view->render($response, 'Respostas.twig');
+    return $view->render($response, 'Respostas.twig', [
+      'title' => 'Respostas',
+      'questions' => array_combine($this->questions, $this->answers),
+    ]);
   }
 
 }
